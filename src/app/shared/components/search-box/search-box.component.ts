9c7a01b927c1e73e3f99591823a34c7a2b3c8137
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-02-05 14:44:36 -0500

    feat: :sparkles: Added debounce functionality to seacrbox in by-capital page

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,16 +7,35 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>();
 
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter();
+
   @Input()
   public placeholder: string = '';
 
+  ngOnInit(): void {
+    this.debouncer
+      .pipe( 
+        debounceTime(600) 
+      )
+      .subscribe( value => {
+        this.onDebounce.emit( value )
+      })
+  }
+
   emitValue(value: string): void {
-    this.onValue.emit( value )
+    this.onValue.emit( value );
+  }
+
+  onKeyPress( searchTerm: string ) {
+    this.debouncer.next( searchTerm );
   }
 
 
